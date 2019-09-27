e0532a42d75e5ad46641e6221b5c58bfcef79087
Sjomullen <54435499+Sjomullen@users.noreply.github.com>
2019-09-27 09:30:11 +0200

    Gjorde klart uppgift 1

---
+++ Metoder 2 uppgift 1/Metoder 2 uppgift 1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Metoder_2_uppgift_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10, b = 20;
+            BytPlats(ref a, ref b); 
+            Console.WriteLine(a + " " + b);
+        }
+        public static void BytPlats(ref int a, ref int b)
+        {
+            int nummer1 = a;
+            int nummer2 = b;
+            b = nummer1;
+            a = nummer2;
+        }
+    }
+}
